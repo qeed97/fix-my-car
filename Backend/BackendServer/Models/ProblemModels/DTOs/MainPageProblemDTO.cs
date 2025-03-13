dd59ec93ec448477f8d16ea6e859c54435c8332c
qeed97 <qeed97@gmail.com>
2025-03-13 23:13:32 +0100

    added main page problems model

---
+++ Backend/BackendServer/Models/ProblemModels/DTOs/MainPageProblemDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BackendServer.Models.ProblemModels.DTOs;
+
+public class MainPageProblemDTO
+{
+    public List<ProblemDTO> Problems { get; set; }
+    
+    public int Index { get; set; }
+}
