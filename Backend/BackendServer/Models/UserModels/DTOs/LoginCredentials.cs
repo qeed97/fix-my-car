ce8ea67b4b935646d8cebc40797d57114620456a
qeed97 <qeed97@gmail.com>
2025-03-13 15:59:38 +0100

    Created user models

---
+++ Backend/BackendServer/Models/UserModels/DTOs/LoginCredentials.cs
@@ -0,0 +1,7 @@
+﻿namespace BackendServer.Models.UserModels.DTOs;
+
+public class LoginCredentials
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ Backend/BackendServer/Models/UserModels/DTOs/PublicUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using BackendServer.Models.ProblemModels;
+using BackendServer.Models.ProblemModels.DTOs;
+
+namespace BackendServer.Models.UserModels.DTOs;
+
+public class PublicUserDTO
+{
+    public string Username { get; set; }
+    public int Karma { get; set; }
+    public List<ProblemDTO> Problems { get; set; } = [];
+    //public List<Fix> Fixes { get; set; } = [];
+    
+}
