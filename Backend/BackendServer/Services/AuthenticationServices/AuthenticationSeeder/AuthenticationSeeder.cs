a3ef13154a4ac53add1236349fe53e8b09f50649
qeed97 <qeed97@gmail.com>
2025-03-13 23:11:41 +0100

    added token service and auth seeder

---
+++ Backend/BackendServer/Services/AuthenticationServices/AuthenticationSeeder/AuthenticationSeeder.cs
@@ -0,0 +1,44 @@
+﻿using BackendServer.Models.UserModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace BackendServer.Services.AuthenticationServices.AuthenticationSeeder;
+
+public class AuthenticationSeeder(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+{
+    public void SeedRoles()
+    {
+        var tAdminRole = CreateAdminRole();
+        tAdminRole.Wait();
+        var tUserRole = CreateUserRole();
+        tUserRole.Wait();
+        CreateAdminIfNotExists().Wait();
+    }
+
+    private async Task CreateAdminRole()
+    {
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+
+    private async Task CreateUserRole()
+    {
+        await roleManager.CreateAsync(new IdentityRole("User"));
+    }
+
+    private async Task CreateAdminIfNotExists()
+    {
+        var adminInDb = await userManager.FindByEmailAsync("admin@adnim.com");
+        if (adminInDb == null)
+        {
+            var admin = new User
+            {
+                UserName = "admin", Email = "admin@adnim.com", Karma = 1000000,
+            };
+            var adminCreated = await userManager.CreateAsync(admin, "admin123");
+
+            if (adminCreated.Succeeded)
+            {
+                await userManager.AddToRoleAsync(admin, "Admin");
+            }
+        }
+    }
+}
---
+++ Backend/BackendServer/Services/AuthenticationServices/TokenService/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using BackendServer.Models.UserModels;
+
+namespace BackendServer.Services.AuthenticationServices.TokenService;
+
+public interface ITokenService
+{
+    public string CreateToken(User user, string role);
+}
