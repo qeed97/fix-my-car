f0c0c5c355ef3a10241fe8cfddc15a81c81dbd01
qeed97 <qeed97@gmail.com>
2025-03-13 23:14:56 +0100

    registered everything in program.cs and fixed an issue with mysql ef framework by changing to pomelo

--- Backend/BackendServer/Program.cs
+++ Backend/BackendServer/Program.cs
@@ -1,31 +1,55 @@
+using System.Text;
 using BackendServer.Data;
 using BackendServer.Models.UserModels;
+using BackendServer.Services.AuthenticationServices.AuthenticationSeeder;
+using BackendServer.Services.AuthenticationServices.TokenService;
 using BackendServer.Services.ProblemServices.Factory;
 using BackendServer.Services.ProblemServices.Repository;
 using BackendServer.Services.UserServices.Factory;
 using BackendServer.Services.UserServices.Repository;
 using Microsoft.EntityFrameworkCore;
 using DotNetEnv;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-Env.Load();
+Env.Load("../../.env");
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddSwaggerGen(c =>
+builder.Services.AddSwaggerGen(option =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Title = "Backend API",
-        Version = "v1",
-        Description = "API for managing car-related issues",
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            []
+        }
     });
 });
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<ApiDbContext>(options => { options.UseMySQL(GetConnString()); });
+builder.Services.AddDbContext<ApiDbContext>(options => { options.UseMySql(GetConnString(), new MySqlServerVersion(new Version(8, 0, 30))); });
 
 builder.Services.AddIdentityCore<User>(options =>
     {
@@ -42,8 +66,12 @@ builder.Services.AddIdentityCore<User>(options =>
 
 builder.Services.AddSingleton<IUserFactory, UserFactory>();
 builder.Services.AddSingleton<IProblemFactory, ProblemFactory>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IProblemRepository, ProblemRepository>();
+builder.Services.AddScoped<AuthenticationSeeder>();
+
+AddJwt();
 
 var app = builder.Build();
 
@@ -51,7 +79,9 @@ using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
 var context = services.GetRequiredService<ApiDbContext>();
-//if (context.Database.IsRelational()) context.Database.Migrate();
+if (context.Database.IsRelational()) context.Database.Migrate();
+var authenticationSeeder = services.GetRequiredService<AuthenticationSeeder>();
+authenticationSeeder.SeedRoles();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -66,6 +96,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+    
 app.UseAuthorization();
 
 app.MapControllers();
@@ -86,4 +118,38 @@ string GetConnString()
     return connectionString;
 }
 
+void AddJwt()
+{
+    var issuingKey = "";
+    if (builder.Environment.IsEnvironment("Testing"))
+    {
+        issuingKey = "dfhuiaozt478356784ztwuerz7ö3qwtzrugfhsoi";
+    }
+    else
+    {
+        issuingKey = Environment.GetEnvironmentVariable("ISSUING_KEY") ??
+                     throw new Exception("ISSUING_KEY not found");
+    }
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "fixmy",
+                ValidAudience = "fixmy",
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(issuingKey)
+                )
+            };
+        });
+}
+
 app.Run();
+
+public partial class Program { }
