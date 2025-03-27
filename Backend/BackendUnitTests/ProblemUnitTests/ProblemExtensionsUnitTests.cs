a73b4223d89e7732d87c3e19e157afa29e21795d
qeed97 <qeed97@gmail.com>
2025-03-27 18:25:55 +0100

    added unit tests for problem extensions and factory

---
+++ Backend/BackendUnitTests/ProblemUnitTests/ProblemExtensionsUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using BackendServer.Extensions;
+using BackendServer.Models.FixModel;
+using BackendServer.Models.ProblemModels;
+using BackendServer.Models.UserModels;
+
+namespace BackendUnitTests.ProblemUnitTests;
+
+[TestFixture]
+public class ProblemExtensionsUnitTests
+{
+    [Test]
+    public void ToDTO_ConvertsCorrectly()
+    {
+        var user = new User { Id = "userId", UserName = "userName" };
+        var fixes = new List<Fix>();
+
+        var problem = new Problem
+        {
+            Id = Guid.NewGuid(),
+            Description = "This is a test",
+            Title = "This is a test",
+            Fixes = fixes,
+            User = user,
+            UserId = user.Id,
+        };
+
+        var dto = problem.ToDTO();
+        
+        Assert.That(dto.Id, Is.EqualTo(problem.Id));
+        Assert.That(dto.Description, Is.EqualTo(problem.Description));
+        Assert.That(dto.Title, Is.EqualTo(problem.Title));
+        Assert.That(dto.Username, Is.EqualTo(user.UserName));
+    }
+
+    [Test]
+    public void ToDTO_ThrowsException_WhenUserNull()
+    {
+        var fixes = new List<Fix>();
+        var problem = new Problem
+        {
+            Id = Guid.NewGuid(),
+            Description = "This is a test",
+            Title = "This is a test",
+            Fixes = fixes,
+            User = null,
+            UserId = null
+        };
+        
+        Assert.Throws<NullReferenceException>(() => problem.ToDTO());
+    }
+    
+}
---
+++ Backend/BackendUnitTests/ProblemUnitTests/ProblemFactoryUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using BackendServer.Models.FixModel;
+using BackendServer.Models.ProblemModels;
+using BackendServer.Models.ProblemModels.DTOs;
+using BackendServer.Models.UserModels;
+using BackendServer.Services.ProblemServices.Factory;
+
+namespace BackendUnitTests.ProblemUnitTests;
+
+[TestFixture]
+public class ProblemFactoryUnitTests
+{
+    [Test]
+    public void UpdateProblem_UpdatesExistingProblem()
+    {
+        var factory = new ProblemFactory();
+        var problem = new Problem { Id = Guid.NewGuid(), Description = "This is a problem" , Title = "This is a problem", Fixes = new List<Fix>(), User = new User{ Id = "User1" }, UserId = "User1" };
+        var updatedProblem = new UpdatedProblem{ Title = "updated title", Description = "updated description"};
+        
+        var newUpdatedProblem = factory.CreateNewUpdatedProblem(updatedProblem, problem);
+        
+        Assert.That(newUpdatedProblem.Description, Is.EqualTo("updated description"));
+        Assert.That(newUpdatedProblem.Title, Is.EqualTo("updated title"));
+    }
+}
