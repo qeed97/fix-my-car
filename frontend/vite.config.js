7c08a84db2da33ece5e84078d4eb14b720bfcb8f
qeed97 <qeed97@gmail.com>
2025-04-02 13:09:55 +0200

    added tailwind to the project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,22 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+    plugins: [
       tailwindcss(),
       react()],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true,
+        strictPort: true,
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://backend-server:8080/',
+                changeOrigin: true,
+                rewrite: path => path.replace(/^\/api/, '')
+            }
+        }
+    }
 })
