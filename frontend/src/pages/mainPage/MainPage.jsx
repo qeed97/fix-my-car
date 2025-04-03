23e19c77f08ecd99405d576be8102a277a7de260
qeed97 <qeed97@gmail.com>
2025-04-03 23:08:45 +0200

    mainpage created with visible problems

---
+++ frontend/src/pages/mainPage/MainPage.jsx
@@ -0,0 +1,50 @@
+﻿import React, {useEffect, useState} from 'react';
+import {useCookies} from "react-cookie";
+import ProblemComponent from "../../components/problemComponent/ProblemComponent.jsx";
+
+export default function MainPage({ searchProblem, normalProblem, setsearchProblem, setNormalProblem, /*setUserLoginCookies*/ }) {
+    const [startIndex, setStartIndex] = useState(0);
+    //const [cookies] = useCookies(['user']);
+    const [allProblems, setAllProblems] = useState(false);
+
+    const fetchProblems = async () => {
+        try {
+            if (!allProblems) {
+                const res = await fetch(`/api/Problems?startIndex=${startIndex}`, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                    }
+                });
+                const data = await res.json();
+                console.log(data);
+                if (data.problems.length === 0){
+                    setAllProblems(() => true);
+                    return;
+                }
+                setNormalProblem(() => [...normalProblem, ...data.problems]);
+                setsearchProblem(() => [...searchProblem, ...data.problems]);
+                setStartIndex(() => data.index);
+            }
+        } catch (error){
+            console.log(error)
+        }
+    };
+
+    useEffect(() => {
+        setNormalProblem(() => []);
+        setsearchProblem(() => []);
+        fetchProblems();
+    }, []);
+
+    return searchProblem ? (
+        <>
+            <div>
+                <ProblemComponent problems={searchProblem}/>
+            </div>
+        </> ):
+        <div>
+            Lading
+        </div>
+
+};
