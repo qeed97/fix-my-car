9f381e2190f1989263e7bb4f40603983889e1866
qeed97 <qeed97@gmail.com>
2025-04-22 18:16:17 +0200

    added logo and created navbutton component

---
+++ frontend/src/components/navButton/NavButton.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import {useLocation, useNavigate} from "react-router-dom";
+
+export default function NavButton({ to, label , setOpen }) {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    return (
+        <button
+            onClick={() => {
+                navigate(to);
+                setOpen(false);
+            }}
+            className={`text-[2.2vh] tracking-wider hover:text-yellow-300 ${
+                location.pathname === to ? "underline" : ""
+            }`}
+        >
+            {label}
+        </button>
+    );
+}
