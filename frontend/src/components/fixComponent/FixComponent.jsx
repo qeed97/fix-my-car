8e3c8487b60475309b19366cb6b1e1629cc1830d
qeed97 <qeed97@gmail.com>
2025-05-06 20:45:00 +0200

    no possible to see fixes

---
+++ frontend/src/components/fixComponent/FixComponent.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import {toast} from "react-hot-toast";
+import {useNavigate} from "react-router-dom";
+
+export default function FixComponent({fix,problem,setFixes,setProblemData}) {
+    const showErrorToast = (message) => toast.error(message);
+    const navigate = useNavigate();
+    //const deleteFix = async () => {}
+
+    return (
+        <div key={fix.id}
+        className="relative w-screen h-[17vh] bg-[#000000aa] backdrop-blur-md border-4 border-yellow-300 p-[2vh] rounded-md shadow-[0_0_20px_5px_rgba(255,255,0,0.8)]">
+            <p className="h-[5vh] text-[2vh] bg-black text-lime-300 border border-lime-400 p-[1vh] break-words">{fix.content}</p>
+        </div>
+    );
+}
